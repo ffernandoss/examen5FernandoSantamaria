3b8ed71a99539f0b31912101606b1d2d065d4593
ffernandoss <fsantama@myuax.com>
2024-05-13 15:32:38 +0200

    modificacion restaurante mediator y libre

--- src/main/java/entrega3/Libre.java
+++ src/main/java/entrega3/Libre.java
@@ -3,6 +3,6 @@ package entrega3;
 public class Libre implements EstadoMesa {
     @Override
     public void manejar(Mesa mesa) {
-        // Código para manejar estado libre
+        System.out.println("La mesa está libre");
     }
 }
--- src/main/java/entrega3/RestauranteMediator.java
+++ src/main/java/entrega3/RestauranteMediator.java
@@ -1,8 +1,18 @@
 package entrega3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RestauranteMediator implements Mediator{
+    private List<Componente> componentes;
+
+    public RestauranteMediator() {
+        this.componentes = new ArrayList<>();
+    }
+
     @Override
     public void addComponente(Componente componente){
+        componentes.add(componente);
         System.out.println("Componente agregado");
     }
 
