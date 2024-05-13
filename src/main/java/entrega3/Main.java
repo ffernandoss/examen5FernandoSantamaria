86483184d6f3b8a88b7c95c7c23a565122103ba1
ffernandoss <fsantama@myuax.com>
2024-05-13 15:30:37 +0200

    main entrega 3

--- src/main/java/entrega3/Main.java
+++ src/main/java/entrega3/Main.java
@@ -3,5 +3,37 @@ package entrega3;
 public class Main {
     public static void main(String[] args) {
 
+        Mediator mediator = new RestauranteMediator();
+
+
+        Componente cocina = new Cocina(mediator);
+        Componente servicioAlCliente = new ServicioAlCliente(mediator);
+        Componente entrega = new Entrega(mediator);
+
+
+        mediator.addComponente(cocina);
+        mediator.addComponente(servicioAlCliente);
+        mediator.addComponente(entrega);
+
+
+        Command reservarCommand = new ReservarCommand();
+        Command ordenarCommand = new OrdenarCommand();
+        Command cancelarCommand = new CancelarCommand();
+
+
+        reservarCommand.execute();
+        ordenarCommand.execute();
+        cancelarCommand.execute();
+
+
+        Mesa mesa = new Mesa();
+        mesa.setEstado(new Libre());
+        mesa.manejarEstado();
+
+        mesa.setEstado(new Reservada());
+        mesa.manejarEstado();
+
+        mesa.setEstado(new Ocupada());
+        mesa.manejarEstado();
     }
 }
