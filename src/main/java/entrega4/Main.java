e078f845d85d5068352fbd5eab5d8e0292c383e9
ffernandoss <fsantama@myuax.com>
2024-05-13 15:46:19 +0200

    main entrega4

--- src/main/java/entrega4/Main.java
+++ src/main/java/entrega4/Main.java
@@ -1,7 +1,26 @@
 package entrega4;
 
 public class Main {
-    public static void arg(String[] args) {
+    public static void main(String[] args) {
 
+        Restaurante restaurante1 = new Restaurante("Restaurante 1");
+        Restaurante restaurante2 = new Restaurante("Restaurante 2");
+        ComponenteRestaurante elemento1 = new ElementoRestaurante("Elemento 1");
+        ComponenteRestaurante elemento2 = new ElementoRestaurante("Elemento 2");
+
+
+        restaurante1.agregar(elemento1);
+        restaurante2.agregar(elemento2);
+
+
+        ModeloRestaurante modelo = new ModeloRestaurante(restaurante1);
+
+
+        VistaRestaurante vista = new VistaRestaurante();
+
+
+        ControladorRestaurante controlador = new ControladorRestaurante(modelo, vista);
+
+        controlador.agregarComponente(restaurante2);
     }
 }
